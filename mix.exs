b74234b3abc674173526b58f1498868c750a9ad1
dmitry-sidorov <jimmaggot@mail.ru>
2024-10-29 12:07:52 +0300

    Add UI lib

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule BudgetManager.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:mishka_chelekom, "~> 0.0.1", only: :dev}
     ]
   end
 
